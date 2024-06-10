3ef87b7fe8e29552da6ab9c707b9b5b78d175214
akia williams <acwill0@gmail.com>
2024-06-10 19:18:01 -0400

    operators

---
+++ class3-boolean-operators/operators-boolean-expressions-comment.py
@@ -0,0 +1,85 @@
+# Operators, boolean Epressions and Comments
+
+# I am a comment
+temperatrue = 95 # I am an inline comment
+
+# I am a standard single line comment
+first_name = 'Thomas' ''' Multi line comment - inline'''
+last_name = 'the Train' """ Inline comment with double quotes"""
+
+'''
+Everything between these quotes will be commente out
+'''
+
+# comment me
+
+#length = 5
+#width = 7
+
+#perimeter = 2 * (length + width) #This is a perimeter of a rectangle
+
+#print(perimeter) 
+
+
+#converting fahrenheit to celsius
+
+#fahrenheit = 89 
+
+#celsius = (fahrenheit - 32) * 5/9 
+
+
+#print(celsius)
+
+# Add 5 to me
+
+age = 25
+age += 5 # adding 5 via shortcut operators
+#print(age)
+
+# Adding 10 years
+
+year = 2024
+year += 10
+#print(year)
+
+
+#Subtract 20
+num_one = 55
+num_one -= 20
+#print(num_one)
+
+#Subtract 15
+num_two = 11
+num_two -= 15
+#print(num_two)
+
+# Multiply by 3
+my_value = 9
+my_value *= 3
+print(my_value)
+# Multiply by 10
+mileage = 15
+mileage *= 10
+print(mileage)
+
+#Divide by 2
+pizza_slices = 8
+pizza_slices /= 2
+print(pizza_slices)
+
+# Raise to the 3rd power **
+num_three = 6
+num_three **=3
+print(num_three)
+# Raise to the 2nd power **
+data = 16
+data **=2 
+print(data)
+# Integer division, how many times does 3 go into 16? //
+val_one = 16
+val_one //= 3
+print(val_one)
+# Integer divide by 4 //
+val_two = 9
+val_two //= 4
+print(val_two)
---
+++ class3-boolean-operators/students.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# My Student Dictionary
+cooking_class_dict = {"Name":['Sarah', 'Joe', 'Alice'], "Favorite Food":['Pizza', 'Ice Cream', 'Mango']}
+
+# What does this look like
+students_df = pd.DataFrame(cooking_class_dict)
+print(students_df)
+
+# output
+
+students_df.to_excel('output.xlsx', index=False)
