398ed6481aae953a29689ba25a062e1b371e83eb
akia williams <acwill0@gmail.com>
2024-06-10 20:30:55 -0400

    nore notes

--- class3-boolean-operators/operators-boolean-expressions-comment.py
+++ class3-boolean-operators/operators-boolean-expressions-comment.py
@@ -70,16 +70,87 @@ print(pizza_slices)
 # Raise to the 3rd power **
 num_three = 6
 num_three **=3
-print(num_three)
+#print(num_three)
+
 # Raise to the 2nd power **
 data = 16
 data **=2 
-print(data)
+#print(data)
+
 # Integer division, how many times does 3 go into 16? //
 val_one = 16
 val_one //= 3
-print(val_one)
+#print(val_one)
+
 # Integer divide by 4 //
 val_two = 9
 val_two //= 4
-print(val_two)
+#print(val_two)
+
+
+#Modulus we use often to find if a value is odd or even 
+# Find the remainder if divided by 3 %
+val_three = 10
+val_three %= 3
+#print(val_three)
+
+# Find the remainder if divided by 5 %
+val_four = 14
+val_four %= 5
+#print(val_four)
+
+# Is 6 greater than 2? >
+print(6 > 2)
+result =(6 > 2)
+print('Is 6 greater than 2?')
+
+# Is 5 greater than or equal to 6? >=
+print(5 >= 6)
+result = (5 >= 6)
+print('Is 5 greater than or equal to 6?')
+
+# Is 5 equal to 5? ==
+print(5 == 5)
+result = (5 == 5)
+print('Is 5 equal to 5?')
+
+# Is 100 not equal to 75? !=
+print(100 != 75)
+result =(100 != 75)
+print('Is 100 not equal to 75?')
+
+#and
+# print(5 == 5 and 4 == 4) # True
+# print(2 == 2 and 3 == 1) # False
+# print(1 == 2 and 2 == 10) # False
+
+''''''
+log_1 = (5 == 3)
+log_2 = (4 == 7)
+print('Log 1', log_1) #True
+print('Log2', log_2)  #False
+print('log 1 and Log 2',log_1 and log_2)
+
+# or
+print(5 == 5 or 5==3)# True if at least 1 is true
+
+#not 
+is_it_autumn = True 
+print(not is_it_autumn)# inverts the answer
+
+x= 5
+y = 10
+
+# is x less than y?
+#print(x<y)
+
+# Are we the same object. 'is' keyword
+
+fname = 'Taylor'
+first_name = 'Taylor'
+print(fname is first_name)
+print(fname == first_name)
+
+# in
+print('J' in 'January')
+print('F' in March)
